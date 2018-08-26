bfdfb23c8ab3589761a150d8c3fb22c2a142ed82
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-08-27 01:12:05 +0300

    timer, screenSwitch, liveCount

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+const MAX_ROUND_TIME = 30;
+
 /*
   Answer structure
   answer = {
@@ -7,11 +9,11 @@
 }
 */
 const countPoints = (answers, lives) => {
-  if (!(answers && answers.length) || answers.length !== 10 || (lives < 0 || lives > 3) ||
+  if (!Array.isArray(answers) || answers.length !== 10 || (lives < 0 || lives > 3) ||
   answers.slice().filter((x) => x.isCorrect === true).length < 7) {
     return -1;
   }
-  return answers.reduce((points, x) => {
+  return answers.slice().reduce((points, x) => {
     if (x.isCorrect) {
       points += 100;
       let addPointsMultiple = 0;
@@ -26,5 +28,58 @@ const countPoints = (answers, lives) => {
   }, 0) + 50 * lives;
 };
 
+const updateLives = (answer, liveCount) => {
+  if (liveCount < 0 || typeof answer.isCorrect !== `boolean`) {
+    throw new Error(`Invalid input values`);
+  }
+  return liveCount - ((answer.isCorrect) ? 0 : 1);
+};
 
-export {countPoints};
+/*
+  Screen structure
+  screen = {
+    type: 1,
+    ...
+  }
+}
+*/
+const switchScreen = (screens, currentScreenNumber) => {
+  if (!Array.isArray(screens) || currentScreenNumber < 0 || currentScreenNumber > screens.length) {
+    throw new Error(`Invalid input values`);
+  }
+  return (currentScreenNumber === screens.length - 1) ? -1 : currentScreenNumber + 1;
+};
+
+const Timer = function (maxTime = MAX_ROUND_TIME) {
+  if (typeof maxTime !== `number`) {
+    throw new Error(`Invalid type, input value should be number`);
+  }
+  if (maxTime <= 0) {
+    throw new Error(`Invalid input value, should be a positive`);
+  }
+
+  let time = maxTime;
+  let isBlinked = false;
+
+  const tick = () => {
+    if (time > 0) {
+      time--;
+    }
+
+    if (time <= 5) {
+      isBlinked = true;
+    }
+
+    return {
+      time,
+      isBlinked
+    };
+  };
+
+  return {
+    tick
+  };
+};
+
+
+export {countPoints, updateLives, switchScreen, Timer};
--- js/game.test.js
+++ js/game.test.js
@@ -1,5 +1,5 @@
 import {assert} from 'chai';
-import {countPoints} from './game.js';
+import {countPoints, updateLives, switchScreen, Timer} from './game.js';
 
 describe(`Game points counting`, () => {
   it(`should return -1 when answers are less than 10`, () => {
@@ -73,5 +73,63 @@ describe(`Game points counting`, () => {
       {isCorrect: false, time: 15},
       {isCorrect: true, time: 25}];
     assert.equal(800, countPoints(answers, 1));
+  });
+});
+
+describe(`Updating player's lives`, () => {
+  it(`should return exception, invalid data`, () => {
+    assert.throw(() => updateLives(`answer`, 1), `Invalid input values`);
+  });
+  it(`valid data, correct answers`, () => {
+    assert.equal(3, updateLives({isCorrect: true, time: 5}, 3));
+    assert.equal(2, updateLives({isCorrect: true, time: 15}, 2));
+    assert.equal(1, updateLives({isCorrect: true, time: 25}, 1));
+  });
+  it(`valid data, incorrect answers`, () => {
+    assert.equal(2, updateLives({isCorrect: false, time: 5}, 3));
+    assert.equal(1, updateLives({isCorrect: false, time: 15}, 2));
+    assert.equal(0, updateLives({isCorrect: false, time: 25}, 1));
+    assert.equal(-1, updateLives({isCorrect: false, time: 25}, 0));
+  });
+});
+
+describe(`Screens switching`, () => {
+  it(`Invalid data`, () => {
+    assert.throw(() => switchScreen(`answer`, 1), `Invalid input values`);
+  });
+  it(`valid data, correct data`, () => {
+    assert.equal(1, switchScreen([0, 1, 2, 3, 4, 5, 6], 0));
+    assert.equal(2, switchScreen([0, 1, 2, 3, 4, 5, 6], 1));
+    assert.equal(6, switchScreen([0, 1, 2, 3, 4, 5, 6], 5));
+  });
+  it(`valid data, correct data, last screen`, () => {
+    assert.equal(-1, switchScreen([0, 1, 2, 3, 4, 5, 6], 6));
+  });
+});
+
+describe(`Timer`, () => {
+  it(`Invalid data type`, () => {
+    assert.throw(() => new Timer(`answer`), `Invalid type, input value should be number`);
+  });
+  it(`Invalid data value`, () => {
+    assert.throw(() => new Timer(-1), `Invalid input value, should be a positive`);
+  });
+  it(`Default timer`, () => {
+    const timer = new Timer();
+    assert.deepEqual({time: 29, isBlinked: false}, timer.tick());
+  });
+  it(`10 seconds timer`, () => {
+    const timer = new Timer(10);
+    assert.deepEqual({time: 9, isBlinked: false}, timer.tick());
+    assert.deepEqual({time: 8, isBlinked: false}, timer.tick());
+    assert.deepEqual({time: 7, isBlinked: false}, timer.tick());
+    assert.deepEqual({time: 6, isBlinked: false}, timer.tick());
+    assert.deepEqual({time: 5, isBlinked: true}, timer.tick());
+    assert.deepEqual({time: 4, isBlinked: true}, timer.tick());
+    assert.deepEqual({time: 3, isBlinked: true}, timer.tick());
+    assert.deepEqual({time: 2, isBlinked: true}, timer.tick());
+    assert.deepEqual({time: 1, isBlinked: true}, timer.tick());
+    assert.deepEqual({time: 0, isBlinked: true}, timer.tick());
+    assert.deepEqual({time: 0, isBlinked: true}, timer.tick());
   });
 });
