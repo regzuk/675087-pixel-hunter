6b0c35ea3648bac1393bbaffd68e77836708e7ea
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-08-26 22:27:06 +0300

    game poiints counting

--- js/game.js
+++ js/game.js
@@ -1,7 +1,29 @@
-
+/*
+  Answer structure
+  answer = {
+    isCorrect: true,
+    time: `fast`
+  }
+}
+*/
 const countPoints = (answers, lives) => {
-  answers = lives;
-  return -1;
+  if (!(answers && answers.length) || answers.length !== 10 || (lives < 0 || lives > 3) ||
+  answers.slice().filter((x) => x.isCorrect === true).length < 7) {
+    return -1;
+  }
+  return answers.reduce((points, x) => {
+    if (x.isCorrect) {
+      points += 100;
+      let addPointsMultiple = 0;
+      if (x.time === `fast`) {
+        addPointsMultiple = 1;
+      } else if (x.time === `slow`) {
+        addPointsMultiple = -1;
+      }
+      points += addPointsMultiple * 50;
+    }
+    return points;
+  }, 0) + 50 * lives;
 };
 
 
--- js/game.test.js
+++ js/game.test.js
@@ -3,21 +3,75 @@ import {countPoints} from './game.js';
 
 describe(`Game points counting`, () => {
   it(`should return -1 when answers are less than 10`, () => {
-    assert.equal(-1, countPoints());
+    let answers = [{isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`}];
+    assert.equal(-1, countPoints(answers, 3));
   });
   it(`should return 1150 when 10 correct answers (all middle time), 3 lives`, () => {
-    assert.equal(1150, countPoints());
+    let answers = [{isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `middle`}];
+    assert.equal(1150, countPoints(answers, 3));
   });
   it(`should return -1 when 6 correct answers, 0 lives`, () => {
-    assert.equal(-1, countPoints());
+    let answers = [{isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: false, time: `slow`},
+      {isCorrect: false, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: false, time: `slow`},
+      {isCorrect: false, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`}];
+    assert.equal(-1, countPoints(answers, 0));
   });
   it(`should return 1650 when 10 correct answers with fast time, 3 lives`, () => {
-    assert.equal(1650, countPoints());
+    let answers = [{isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `fast`}];
+    assert.equal(1650, countPoints(answers, 3));
   });
   it(`should return 650 when 10 correct answers with slow time, 3 lives`, () => {
-    assert.equal(650, countPoints());
+    let answers = [{isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: true, time: `slow`}];
+    assert.equal(650, countPoints(answers, 3));
   });
   it(`should return 800 when 8 correct answers, 2 with fast time, 3 with middle time, 3 with slow time, 1 lives`, () => {
-    assert.equal(800, countPoints());
+    let answers = [{isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: false, time: `middle`},
+      {isCorrect: true, time: `fast`},
+      {isCorrect: true, time: `middle`},
+      {isCorrect: true, time: `slow`},
+      {isCorrect: false, time: `middle`},
+      {isCorrect: true, time: `slow`}];
+    assert.equal(800, countPoints(answers, 1));
   });
 });
