92654f26e30bf3e6a7d38f7c04f2a0b6ccefb743
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-08-29 00:01:24 +0300

    statTemplate

--- js/stats.js
+++ js/stats.js
@@ -1,12 +1,50 @@
 import {getDomElementFromString} from './util.js';
 import gameStat from './gameStat.js';
 
-// const gameStatRowTemplate = (number) =>
+const FAST_BONUS_STR = `Бонус за скорость`;
+const LIVE_BONUS_STR = `Бонус за жизни`;
+const SLOW_BONUS_STR = `Штраф за медлительность`;
 
-// const statTableTemplate = (number, fastBonus = 0, slowBonus = 0, liveBonus = 0) => {
-//   const table = document.createElement(`table`);
-//   table.classList.add(`result__table`);
-// };
+const gameStatRowTemplate = (number) => {
+  const rowTemplate = getDomElementFromString(`<tr>
+    <td class="result__number">${number}</td>
+    <td colspan="2">
+    </td>
+    <td class="result__points">× 100</td>
+    <td class="result__total">900</td>
+  </tr>`, `table`);
+  rowTemplate.querySelector(`td:not([class])`).appendChild(gameStat());
+  return rowTemplate;
+};
+
+const gameBonusTemplate = (str, imgStr, count) => {
+  const bonusTemplate = getDomElementFromString(`<tr>
+    <td></td>
+    <td class="result__extra">${str}:</td>
+    <td class="result__extra">${Math.abs(count)} <span class="stats__result stats__result--${imgStr}"></span></td>
+    <td class="result__points">× 50</td>
+    <td class="result__total">${count * 50}</td>
+  </tr>`, `table`);
+  return bonusTemplate;
+};
+
+const statTableTemplate = (number, result, fastBonus = 0, slowBonus = 0, liveBonus = 0) => {
+  const table = document.createElement(`table`);
+  table.classList.add(`result__table`);
+  table.appendChild(gameStatRowTemplate(number));
+  if (fastBonus > 0) {
+    table.appendChild(gameBonusTemplate(FAST_BONUS_STR, `fast`, fastBonus));
+  }
+  if (liveBonus > 0) {
+    table.appendChild(gameBonusTemplate(LIVE_BONUS_STR, `alive`, liveBonus));
+  }
+  if (slowBonus > 0) {
+    table.appendChild(gameBonusTemplate(SLOW_BONUS_STR, `slow`, slowBonus));
+  }
+  table.appendChild(getDomElementFromString(`<tr>
+    <td colspan="5" class="result__total  result__total--final">${result}</td>
+  </tr>`));
+};
 
 const screenTemplate = getDomElementFromString(`<section class="result">
   <h2 class="result__title">Победа!</h2>
@@ -73,8 +111,10 @@ const screenTemplate = getDomElementFromString(`<section class="result">
   </table>
 </section>`);
 
-Array.from(screenTemplate.querySelectorAll(`.result__table`))
-.forEach((x) => x.querySelector(`tr:first-child`).querySelector(`td:not([class])`).appendChild(gameStat()));
+screenTemplate.appendChild(statTableTemplate(1, 950, 1, 1, 2));
+
+// Array.from(screenTemplate.querySelectorAll(`.result__table`))
+// .forEach((x) => x.querySelector(`tr:first-child`).querySelector(`td:not([class])`).appendChild(gameStat()));
 
 const getScreen = () => {
   const screen = screenTemplate.cloneNode(true);
--- js/util.js
+++ js/util.js
@@ -1,5 +1,5 @@
-const getDomElementFromString = (string) => {
-  const div = document.createElement(`div`);
+const getDomElementFromString = (string, tag = `div`) => {
+  const div = document.createElement(tag);
   div.insertAdjacentHTML(`afterbegin`, string);
 
   if (div.childNodes.length === 1) {
