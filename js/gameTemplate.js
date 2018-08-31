765d101f3361f0cf28981019e218a6676f9aac85
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-08-31 16:43:56 +0300

    gameTemplate

--- js/gameTemplate.js
+++ js/gameTemplate.js
@@ -33,18 +33,35 @@ const getScreen = (question) => {
   const gameTask = screen.querySelector(`.game__task`);
   gameTask.text = GAME_TASK[question.type];
   switch (question.type) {
-    case 1:
-      question.img.forEach((x, i) => gameForm.appendChild(getGameOptionTemplate(`question${i + 1}`, IMG_SIZE[question.type].width, IMG_SIZE[question.type].height, x.src)));
-      break;
     case 2:
       gameForm.classList.add(`game__content--wide`);
-      gameForm.appendChild(getGameOptionTemplate(`question1`, IMG_SIZE[question.type].width, IMG_SIZE[question.type].height, question.img.src));
       break;
     case 3:
       gameForm.classList.add(`game__content--triple`);
-      question.img.forEach((x, i) => gameForm.appendChild(getGameOptionTemplate(`question${i + 1}`, IMG_SIZE[question.type].width, IMG_SIZE[question.type].height, x.src)));
       break;
   }
+  const options = [];
+  question.img.forEach((x, i) => {
+    options.push(getGameOptionTemplate(`question${i + 1}`, IMG_SIZE[question.type].width, IMG_SIZE[question.type].height, x.src));
+    gameForm.appendChild(options[i]);
+  });
+
+  if (question.type === 3) {
+    Array.from(screen.querySelectorAll(`.game__option`)).forEach((x) => x.addEventListener(`click`, () => {
+      x.classList.add(`game__option--selected`);
+      selectScreen(`stat`);
+    }));
+  } else {
+    const onRadioChange = (qOptions) => {
+      if (qOptions[0].some((x) => x.checked) && qOptions[1].some((x) => x.checked)) {
+        selectScreen(`gameTwo`);
+      }
+    };
+    options.map((x, i) => Array.from(x.querySelectorAll(`input[name=question${i + 1}]`))).forEach((y) => y.forEach((z) => z.addEventListener(`change`, () => {
+      onRadioChange(y);
+    })));
+  }
+  screen.appendChild(gameStat());
 };
 
 export default getScreen;
