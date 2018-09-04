4ca6d3ca11b80fa5178584b2ff02a5a0c176f816
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-09-05 00:16:43 +0300

    add resize function

--- js/gameOptionTemplate.js
+++ js/gameOptionTemplate.js
@@ -1,5 +1,6 @@
 import {getDomElementFromString} from './util.js';
-export default (nameOption, width, height, imgSrc, isTriple = false) => (isTriple) ? getDomElementFromString(`<div class="game__option">
+export default (nameOption, width, height, imgSrc, isTriple = false) => {
+  const option = (isTriple) ? getDomElementFromString(`<div class="game__option">
   <img src="${imgSrc}" alt="${nameOption}" width="${width}" height="${height}">
 </div>`) : getDomElementFromString(`<div class="game__option">
     <img src="${imgSrc}" alt="${nameOption}" width="${width}" height="${height}">
@@ -12,3 +13,19 @@ export default (nameOption, width, height, imgSrc, isTriple = false) => (isTripl
       <span>Рисунок</span>
     </label>
   </div>`);
+  const image = option.querySelector(`img`);
+  image.addEventListener(`load`, () => {
+    const size = resize({width, height}, {width: image.naturalWidth, height: image.naturalHeight});
+    image.width = size.width;
+    image.height = size.height;
+  });
+  return option;
+};
+
+const resize = (screen, img) => {
+  if (screen.width >= img.width) {
+    return img;
+  }
+  const r = screen.width / img.width;
+  return {width: screen.width, height: Math.floor(img.height * r)};
+};
