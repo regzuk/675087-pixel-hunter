d89983932aa8b00597efb9b1c2e7f7d68c0c8602
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-09-06 22:29:13 +0300

    resize pictures

--- js/gameOptionTemplate.js
+++ js/gameOptionTemplate.js
@@ -1,9 +1,11 @@
 import {getDomElementFromString} from './util.js';
+import {resize} from './resize.js';
+
 export default (nameOption, width, height, imgSrc, isTriple = false) => {
   const option = (isTriple) ? getDomElementFromString(`<div class="game__option">
-  <img src="${imgSrc}" alt="${nameOption}" width="${width}" height="${height}">
+  <img src="${imgSrc}" alt="${nameOption}">
 </div>`) : getDomElementFromString(`<div class="game__option">
-    <img src="${imgSrc}" alt="${nameOption}" width="${width}" height="${height}">
+    <img src="${imgSrc}" alt="${nameOption}">
     <label class="game__answer game__answer--photo">
       <input class="visually-hidden" name="${nameOption}" type="radio" value="photo">
       <span>Фото</span>
@@ -13,5 +15,13 @@ export default (nameOption, width, height, imgSrc, isTriple = false) => {
       <span>Рисунок</span>
     </label>
   </div>`);
+
+  const image = option.querySelector(`img`);
+  image.addEventListener(`load`, () => {
+    const imgSize = resize({width, height}, {width: image.naturalWidth, height: image.naturalHeight});
+    image.width = imgSize.width;
+    image.height = imgSize.height;
+  });
+
   return option;
 };
